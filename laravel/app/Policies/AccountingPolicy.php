0f06b51898eeb92f4e6409d85f675c61d0b36c04
Veronika Andrusyk <andrusykveronika@gmail.com>
2023-12-21 23:30:14 +0200

    Done lab-4

---
+++ laravel/app/Policies/AccountingPolicy.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Accounting;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AccountingPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(?User $user): bool
+    {
+        if($user === null)
+        {
+            return false;
+        }
+        elseif($user->role == "superadmin")
+        {
+            return true;
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Accounting $accounting): bool
+    {
+        if($user->id == $accounting->creator_user_id)
+        {
+            return true;
+        }
+        elseif ($user->role == "superadmin" or $user->role == "editor")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(?User $user, Accounting $accounting): bool
+    {
+        if ($user->role == "superadmin")
+        {
+            return true;
+        }
+        elseif($user->id == $accounting->creator_user_id)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user): bool
+    {
+        return false;
+    }
+}
